1e3445f7d154349f1a3c00ed5231310845399876
liJIREN <George@liJIRENs-MacBook-Pro.local>
2015-11-12 23:08:52 -0800

    level progression image

---
+++ BlockTileSystem/Assets/Scripts/LevelProgressionImage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelProgressionImage : MonoBehaviour {
+	[SerializeField] Sprite[] sprite = new Sprite[16];
+
+
+
+
+	void OnEnable () {
+        Events.g.AddListener<LevelLoadedEvent>(LevelLoaded);
+    }
+    void OnDisable () {
+        Events.g.RemoveListener<LevelLoadedEvent>(LevelLoaded);
+    }
+    void LevelLoaded (LevelLoadedEvent e) {
+
+        	switchImage(sprite[e.iLevel-1]);
+
+    }
+    void switchImage(Sprite sprite)
+    {
+    	gameObject.GetComponent<Image>().sprite=sprite;
+    }
+}
